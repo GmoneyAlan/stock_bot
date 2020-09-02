dc97fa9717cdb554b7329d6e1150e8b84764e4be
GmoneyAlan <alan200@outlook.com>
2020-09-01 20:07:13 -0600

    acquires data on AAPL using DataReader

--- Stock.py
+++ Stock.py
@@ -2,4 +2,13 @@ import pandas as pd
 import pandas_datareader as pdr
 import datetime as dt
 
+start_date = dt.datetime(1995,1,16)
+end_date = dt.datetime(2020, 9, 1)
 
+stock = 'AAPL'
+df = []
+
+df = pdr.DataReader(stock, 'yahoo', start_date, end_date)
+
+
+#print(df.head())
