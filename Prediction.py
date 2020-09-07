9c7889902478f3f7e2a8aa7066064fd02bc2efc1
GmoneyAlan <alan200@outlook.com>
2020-09-07 00:05:03 -0600

    Additional code to Pred..

--- Prediction.py
+++ Prediction.py
@@ -10,9 +10,10 @@ import pandas as pd
 import seaborn as sns
 import tensorflow as tf 
 
+
 class StockGenerator():
     def __init__(self, input_width, label_width, shift,
-                  train_df=train_df, val_df=val_df, test_df=test_df, label_columns=None):
+                  train_df=[], val_df=[], test_df=[], label_columns=None):
         
         self.train_df = train_df
         self.val_df = val_df
@@ -28,11 +29,13 @@ class StockGenerator():
         self.shift = shift
         
         self.total_window_size = input_width + shift
-        self.input_slice = np.arrange(self.total_window_size)[self.input_slice]
+        
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
         
         self.label_start = self.total_window_size - self.label_width
         self.labels_slice = slice(self.label_start, None)
-        self.label_indices = np.arrange(self.total_window_size)[self.labels_slice]
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
         
     def __repr__(self):
         return '\n'.join([
@@ -40,9 +43,55 @@ class StockGenerator():
             f'Input indices: {self.input_indices}',
             f'Label indices: {self.label_indices}',
             f'Label column names: {self.label_columns}'
-        ])
+        ])       
         
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:,:,self.label_columns_indices[name]] for name in self.label_columns],
+                axis=-1
+            )
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+        
+        return inputs, labels
     
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            batch_size=32            
+        )
+        
+        ds = ds.map(self.split_window)
+        return ds
+        
     
 df = pd.read_csv('AAPL.csv')
 #print(df.head())
+#Testing will be done on a 70%, 20%, 10% where
+    #70% = training
+    #20% = validation
+    #10% = test sets
+    
+train_df = df[:int(len(df)*.7)]
+val_df = df[int(len(df)*.7):int(len(df)*.9)]
+test_df = df[int(len(df)*.9):]
+
+
+#Normalize data
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean)/train_std
+val_df = (val_df - train_df) / train_std
+test_df = (test_df - train_mean) / train_std
+s = StockGenerator(input_width=60, label_width=1,shift=60,label_columns=['Close'])
+
+s
